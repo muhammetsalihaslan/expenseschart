a7345d75696ecf07c903e232ff14afce4c3125aa
Your Name <you@example.com>
2024-01-01 15:17:23 +0300

    midde component

--- app/page.js
+++ app/page.js
@@ -13,11 +13,57 @@ export default function Home() {
         </div>
       </div>
       <div className=" bg-[#FFFBF6] w-1/2 h-[25rem] rounded-2xl mt-[1rem]">
-        <div>
-          <h1>Spending - Last 7 days</h1>
+        <div className="mt-7">
+          <h1 className="text-2xl flex justify-center font-bold">
+            Spending - Last 7 days
+          </h1>
         </div>
-        <div></div>
-        <div></div>
+        <div className="flex gap-x-3 items-end justify-center  h-2/5 mt-5   ">
+          <div>
+            <div className="h-[40px] w-[36px] bg-[#EC775F] rounded-md cursor-pointer hover:bg-[#FF9B86] "></div>
+            <h1 className="text-sm mt-3 text-[#98837F] flex items-center justify-center ">
+              mon
+            </h1>
+          </div>
+          <div>
+            <div className="h-[81px] w-[36px] bg-[#EC775F] rounded-md cursor-pointer hover:bg-[#FF9B86] "></div>
+            <h1 className="text-sm mt-3 text-[#98837F] flex items-center justify-center ">
+              tue
+            </h1>
+          </div>
+          <div>
+            <div className="h-[116px] w-[36px] bg-[#76B5BC] rounded-md cursor-pointer hover:bg-[#B4E0E5]"></div>
+            <h1 className="text-sm mt-3 text-[#98837F] flex items-center justify-center ">
+              wed
+            </h1>
+          </div>
+          <div>
+            <div className="h-[72px] w-[36px] bg-[#EC775F] rounded-md cursor-pointer hover:bg-[#FF9B86]"></div>
+            <h1 className="text-sm mt-3 text-[#98837F] flex items-center justify-center ">
+              thu
+            </h1>
+          </div>
+          <div>
+            <div className="h-[54px] w-[36px] bg-[#EC775F] rounded-md cursor-pointer hover:bg-[#FF9B86]"></div>
+            <h1 className="text-sm mt-3 text-[#98837F] flex items-center justify-center ">
+              fri
+            </h1>
+          </div>
+          <div>
+            <div className="h-[100px] w-[36px] bg-[#EC775F] rounded-md cursor-pointer hover:bg-[#FF9B86]"></div>
+            <h1 className="text-sm mt-3 text-[#98837F] flex items-center justify-center ">
+              sat
+            </h1>
+          </div>
+          <div>
+            <div className="h-[59px] w-[36px] bg-[#EC775F] rounded-md cursor-pointer hover:bg-[#FF9B86]"></div>
+            <h1 className="text-sm mt-3 text-[#98837F] flex items-center justify-center ">
+              sun
+            </h1>
+          </div>
+        </div>
+        <hr className="border border-red-200 mx-5 rounded-full" />
+        <div className="bg-red-300 "> part3</div>
       </div>
     </div>
   );
